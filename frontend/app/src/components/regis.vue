8fb48c44a85bf425324fc1ec39f395d1775b5099
yplusone <yangjiani233@outlook.com>
2019-02-16 23:01:48 +0800

    UI

---
+++ frontend/app/src/components/regis.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="centered-container">
+    <md-content class="md-elevation-3">
+
+      <div class="title">
+        <!-- <img src="https://vuematerial.io/assets/logo-color.png"> -->
+        <div class="md-title">ZJUBCA</div>
+        <div class="md-body-1">注册</div>
+      </div>
+
+      <div class="form">
+        <md-field>
+          <label>真实姓名</label>
+          <md-input v-model="studentInfo.FullName" autofocus></md-input>
+        </md-field>
+        <md-field >
+          <label>邮箱</label>
+          <md-input v-model="studentInfo.email" ></md-input>
+        </md-field >
+        <md-field>
+          <label>学号</label>
+          <md-input v-model="studentInfo.studentId"></md-input>
+        </md-field>
+        <md-field >
+          <label>EOS账户</label>
+          <md-input v-model="studentInfo.EosId" ></md-input>
+        </md-field>
+        <md-field md-has-password>
+          <label>password</label>
+          <md-input v-model="studentInfo.password" type="password" ></md-input>
+        </md-field>
+      </div>
+
+      <div class="actions md-layout md-alignment-center-space-between">
+        <md-button class="md-raised md-primary" @click="regis"><router-link to="/" style="color:white;">支付</router-link></md-button>
+        <md-button class="md-raised md-primary" @click="auth">注册</md-button>
+      </div>
+
+      <div class="loading-overlay" v-if="loading">
+        <md-progress-spinner md-mode="indeterminate" :md-stroke="2"></md-progress-spinner>
+      </div>
+
+    </md-content>
+    <div class="background" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      loading: false,
+      studentInfo:{
+        email: "",
+        password: "",
+        studentId:"",
+        EosId:"",
+        FullName:"",
+      }
+    };
+  },
+  methods: {
+    auth() {
+      // your code to login user
+      // this is only for example of loading
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 5000);
+    },
+    regis(){
+
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.centered-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  padding-top: 0vw;
+  .title {
+    text-align: center;
+    margin-bottom: 30px;
+    img {
+      margin-bottom: 16px;
+      max-width: 80px;
+    }
+  }
+  .actions {
+    .md-button {
+      margin: 0;
+    }
+  }
+  .form {
+    margin-bottom: 0px;
+  }
+  .background {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 0;
+  }
+  .md-content {
+    z-index: 1;
+    padding: 40px;
+    width: 100%;
+    max-width: 400px;
+    position: relative;
+  }
+  .loading-overlay {
+    z-index: 10;
+    top: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -13,10 +13,13 @@ import {
 } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import ScatterJS from 'scatterjs-core'
-import ScatterEOS from 'scatterjs-plugin-eosjs'
-import Eos from 'eosjs'
+import infiniteScroll from 'vue-infinite-scroll'
+
+// import ScatterJS from 'scatterjs-core'
+// import ScatterEOS from 'scatterjs-plugin-eosjs'
+// import Eos from 'eosjs'
 import VueMaterial from 'vue-material'
+// import { EIO } from 'constants';
 
 Vue.config.productionTip = false;
 Vue.use(MdButton);
@@ -27,36 +30,37 @@ Vue.use(MdSnackbar);
 Vue.use(MdDialog);
 Vue.use(MdField);
 Vue.use(MdRadio);
-Vue.use(VueMaterial)
+Vue.use(VueMaterial);
+Vue.use(router);
+Vue.use(infiniteScroll);
+// ScatterJS.plugins(new ScatterEOS());
 
-ScatterJS.plugins(new ScatterEOS());
+// ScatterJS.scatter.connect('ZJUBCA.TOKEN', {
+//   initTimeout: 10000,
+// }).then(async connected => {
+//   if (!connected) {
+//     console.log('please unlock your scatter');
+//     return false
+//   }
+//   const network = {
+//     blockchain: 'eos',
+//     protocol: 'https',
+//     host: 'api-kylin.eoslaomao.com',
+//     port: 443,
+//     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'
+//   };
 
-ScatterJS.scatter.connect('ZJUBCA.TOKEN', {
-  initTimeout: 10000,
-}).then(async connected => {
-  if (!connected) {
-    console.log('please unlock your scatter');
-    return false
-  }
-  const network = {
-    blockchain: 'eos',
-    protocol: 'https',
-    host: 'api-kylin.eoslaomao.com',
-    port: 443,
-    chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'
-  };
+//   let scatter = ScatterJS.scatter;
+//   await scatter.getIdentity({accounts: [network]});
+//   const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+//   const eos = scatter.eos(network, Eos, {expireInSeconds: 20});
 
-  let scatter = ScatterJS.scatter;
-  await scatter.getIdentity({accounts: [network]});
-  const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
-  const eos = scatter.eos(network, Eos, {expireInSeconds: 20});
+//   console.log(account)
+//   console.log(eos)
 
-  console.log(account)
-  console.log(eos)
-
-  Vue.prototype.$scatter = scatter;
-  Vue.prototype.$account = account;
-  Vue.prototype.$eos = eos;
+//   Vue.prototype.$scatter = scatter;
+//   Vue.prototype.$account = account;
+//   Vue.prototype.$eos = eos;
 
   // Transaction Example
   // const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
@@ -67,9 +71,32 @@ ScatterJS.scatter.connect('ZJUBCA.TOKEN', {
   // }).catch(error => {
   //   console.error(error);
   // });
-})
+// })
+const EosApi = require('eosjs-api');
+
+const config = {
+  chainId: "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191", // 32 byte (64 char) hex string
+  keyProvider: [], // WIF string or array of keys..
+  httpEndpoint: 'https://api-kylin.eosasia.one',
+  expireInSeconds: 60,
+  broadcast: true,
+  verbose: false, // API activity
+  sign: true
+};
+const eos = EosApi(config);
+
+// let eosinfo=eos.getInfo({}).then(result => console.log(result))
+// var eosinfo;
+// var tt;
+// eos.getInfo({}).then(function(result){console.log(result.chain_id);tt= result.chain_id;return tt;});
+// eosinfo=tt;
+// console.log(tt);
+// eosinfo=result.chainId;
+export{
+  eos
+};
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
